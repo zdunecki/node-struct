d5e3f50d8ff34a6d7458a0248018809dd3cbc198
zdunecki <zdunekhere@gmail.com>
2020-03-31 01:04:34 +0200

    feat: initial version of validation

--- __tests__/testStruct.js
+++ __tests__/testStruct.js
@@ -17,3 +17,31 @@ test("test struct", () => {
   expect(product.FirstName).toBe(firstName);
   expect(product2.FirstName).toBe(firstName);
 });
+
+test("test struct with basic validation", async () => {
+  const Account = struct({
+    ID: `mysql:"id" json:"id" valid:"string,required"`,
+    UserAccountID: `mysql:"user_account_id" json:"userAccountId" valid:"string"`,
+    Email: `mysql:"email" json:"email" valid:"string"`,
+    FirstName: `"mysql":first_name"json:"firstName" valid:"string"`,
+    LastName: `mysql:"last_name" json:"lastName" valid:"string"`,
+    Password: `mysql:"password" json:"password" valid:"string"`,
+    ModificationDate: `mysql:"modification_date" json:"modificationDate valid:"string"`,
+    CreationDate: `mysql:"creation_date" json:"creationDate" valid:"string"`
+  });
+
+  const firstName = "First Name";
+
+  const account = Account({
+    ID: "test-id",
+    FirstName: firstName
+  });
+
+  const account2 = Account(`{"firstName": "${firstName}"}`);
+
+  const { error } = account.isValid();
+
+  expect(account.FirstName).toBe(firstName);
+  expect(account2.FirstName).toBe(firstName);
+  expect(error).toBe(null);
+});
